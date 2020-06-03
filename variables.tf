7bf89ddd7a483b8a1f9a9d064784ea2513e9c1e2
“siva.m01” <siva.manikandakumar@quby.com>
2020-06-03 22:24:56 +0200

    Added new files

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "instance_count" {
+  default = 1
+}
+
+variable "key_name" {
+  description = "Private key name to use with instance"
+  default     = "terraform"
+}
+
+variable "instance_type" {
+  description = "AWS instance type"
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "Base AMI to launch the instances"
+
+  # linux AMI
+  default = "ami-09d95fab7fff3776c"
+}
